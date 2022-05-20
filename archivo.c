da860920c7da3784eabac4c04a21afc947783fb3
Armando  Trout <atroutg@gmail.com>
2022-05-20 17:31:56 -0500

    ultimo commit

--- archivo.c
+++ archivo.c
@@ -3,13 +3,8 @@
 int var1;
 
 int main(){
-<<<<<<< HEAD
 printf("hola, cambio en otra rama");
-
-=======
-printf("hola");
 printf("esta modificacion se hizo en main");
->>>>>>> main
 
 return 0;
 };
